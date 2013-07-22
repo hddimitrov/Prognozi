5c2b95361bdf618da62d28f2c13ba819ba26adee
Radoslav E. Dinev <asdsad@mail.bg>
2013-07-22 08:50:00 +0300

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prognozi::Application.config.secret_token = '16110b1cefcef0b2788242e9de66f71c9812600d596a5472ef234b6375efe06d779b52e458daaf35cb2165e0fb6e40c61c9409d12fec6ce8e5cacb72ee79559b'
