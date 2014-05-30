b8527932f185b8db67ee2e84bac91fc3e1f8c549
hddimitrov <hddimitrov@gmail.com>
2014-05-30 08:13:50 +0300

    knockout stage frontend ready

--- app/assets/javascripts/angular/controllers/predictions.js
+++ app/assets/javascripts/angular/controllers/predictions.js
@@ -3,11 +3,32 @@ angular.module('pro').controller('predictions', ['$scope', 'predictionServices',
     $scope.last_16 = response;
   });
 
+  $scope.quarter_f = {};
+  $scope.semi_f = {};
+  $scope.finalists = {};
+  $scope.champion = {};
+
   $scope.set_group_winner = function(group, team_id, team_name) {
     $scope.last_16.winners[group] = {id: team_id, name: team_name};
   };
 
   $scope.set_group_runner_up = function(group, team_id, team_name) {
     $scope.last_16.runners_up[group] = {id: team_id, name: team_name};
+  };
+
+  $scope.go_through_qf = function(ef, team_id, team_name) {
+    $scope.quarter_f[ef] = {id: team_id, name: team_name};
+  };
+
+  $scope.go_through_sf = function(qf, team_id, team_name) {
+    $scope.semi_f[qf] = {id: team_id, name: team_name};
+  };
+
+  $scope.go_through_f = function(sf, team_id, team_name) {
+    $scope.finalists[sf] = {id: team_id, name: team_name};
+  };
+
+  $scope.win = function(team_id, team_name) {
+    $scope.champion = {id: team_id, name: team_name};
   };
 }]);
