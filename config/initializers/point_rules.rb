aaf463274258126b9df89199790ad77a67c6cb5f
cerebraldeath <hristodd@gmail.com>
2016-05-29 16:24:23 +0300

    top scorer disclaimer

--- config/initializers/point_rules.rb
+++ config/initializers/point_rules.rb
@@ -2,12 +2,12 @@ $current_tournament = 1
 
 $point_rules = OpenStruct.new({
   m_sign_points: 1,
-  m_score_points: 5,
-  gs_position_1_points: 4,
-  gs_position_2_points: 3,
+  m_score_points: 6,
+  gs_position_1_points: 2,
+  gs_position_2_points: 2,
   gs_position_3_points: 2,
-  gs_position_4_points: 1,
-  e_ef_points: 3,
+  gs_position_4_points: 2,
+  e_ef_points: 2,
   e_qf_points: 5,
   e_sf_points: 8,
   e_f_points: 12,
