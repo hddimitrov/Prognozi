5de5314ff6659c537441d14e8da06d65a750511e
cerebraldeath <hristodd@gmail.com>
2016-05-29 16:10:05 +0300

    scoring rules change

--- config/initializers/point_rules.rb
+++ config/initializers/point_rules.rb
@@ -8,9 +8,9 @@ $point_rules = OpenStruct.new({
   gs_position_3_points: 2,
   gs_position_4_points: 1,
   e_ef_points: 3,
-  e_qf_points: 6,
-  e_sf_points: 10,
-  e_f_points: 14,
-  e_c_points: 21,
-  top_scorer_points: 8
+  e_qf_points: 5,
+  e_sf_points: 8,
+  e_f_points: 12,
+  e_c_points: 17,
+  top_scorer_points: 7
 })
